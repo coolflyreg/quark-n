bc9632e84721382e00fe9d04bcd6a45b328633cf
coolflyreg163 <coolflyr_reg@163.com>
2021-01-21 14:31:32 +0800

    LCD Clock添加网络下载速度

--- WorkSpace/Clock/lcd_digital_clock.py
+++ WorkSpace/Clock/lcd_digital_clock.py
@@ -11,6 +11,9 @@ def cputempf():
     StringToOutput = "CPU {0} C".format(int(CPUTemp) /1000.0)
     return StringToOutput
 
+# Return % of CPU used by user as a character string
+def getCPUuse():
+    return (str(os.popen("top -b -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip()))
 
 if not os.getenv('SDL_FBDEV'):
     os.putenv('SDL_FBDEV', '/dev/fb1')#利用quark自带tft屏幕显示
@@ -24,6 +27,24 @@ def get_host_ip():
         s.close()
     return ip
 
+NET_STATS = []
+INTERFACE = 'wlan0'
+def rx():
+    ifstat = open('/proc/net/dev').readlines()
+    for interface in  ifstat:
+        if INTERFACE in interface:
+            stat = float(interface.split()[1])
+            NET_STATS[0:] = [stat]
+
+def tx():
+    ifstat = open('/proc/net/dev').readlines()
+    for interface in  ifstat:
+        if INTERFACE in interface:
+            stat = float(interface.split()[9])
+            NET_STATS[1:] = [stat]
+rx()
+tx()
+
 pygame.init()
 
 # icon = pygame.image.load('digitalClock.png')
@@ -36,12 +57,13 @@ window_size=(w,h)
 screen = pygame.display.set_mode(window_size, pygame.FULLSCREEN)
 pygame.mouse.set_visible( False )
 # pygame.display.set_caption('Digital Clock')
-print(os.path.dirname(__file__))
+print('fontfile path: ', os.path.dirname(__file__))
 fontPath = os.path.dirname(__file__) + "/fonts/DS-DIGIT.TTF"
 bigFont = pygame.font.Font(fontPath, 82)
 middleFont = pygame.font.Font(fontPath, 40)
 smallFont = pygame.font.Font(fontPath, 30)
 miniFont = pygame.font.Font(fontPath, 26)
+tinyFont = pygame.font.Font(fontPath, 24)
 
 # bigFont = pygame.font.SysFont('DS-Digital',130)#Comic Sans MS
 # smallFont = pygame.font.SysFont('DS-Digital',30)
@@ -49,12 +71,17 @@ miniFont = pygame.font.Font(fontPath, 26)
 white = (255,255,255)
 black = (0,0,0)
 green = (0,255,0)
+red = (255,0,0)
 
 months = ['January', 'February', 'March', 'April', 'May', 
 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ]
 
 cputemp = cputempf()
+cpuUse = getCPUuse()
+netShowType = 0
+netShowTypeRemain = 5
+prevSecondIntValue = 0
 running = True
 while running:
     screen.fill(black)
@@ -66,7 +93,8 @@ while running:
         elif event.type == pygame.KEYDOWN:
             running = False
             pygame.quit()
-            
+
+
     now = datetime.now()
     today = datetime.today()
 
@@ -78,9 +106,26 @@ while running:
     year = now.strftime("%Y")
     day = today.weekday()
     day = days[day]
-    
-    if int(second) % 5 == 0:
+    secondIntValue = int(second)
+    if prevSecondIntValue != secondIntValue:
+        rxstat_o = list(NET_STATS)
+        rx()
+        tx()
+        RX = float(NET_STATS[0])
+        RX_O = rxstat_o[0]
+        TX = float(NET_STATS[1])
+        TX_O = rxstat_o[1]
+        RX_RATE = round((RX - RX_O)/1024/1024,3)
+        TX_RATE = round((TX - TX_O)/1024/1024,3)
+    if secondIntValue % 5 == 0:
         cputemp = cputempf()
+        cpuUse = getCPUuse()
+        
+        # netShowTypeRemain = netShowTypeRemain - 1
+        # if netShowTypeRemain == 0:
+        #     netShowTypeRemain = 5
+        #     netShowType = 1 - netShowType
+        # print(netShowTypeRemain, netShowType)
     #month = months[month-1]
     
     am = 'AM'
@@ -100,18 +145,29 @@ while running:
     dayText = smallFont.render( day,True,green)
 
     cputempText = smallFont.render(cputemp, True, white)
+    cpuUseText = smallFont.render(cpuUse + '%', True, white)
+    netSpeedInText = tinyFont.render('' + str(RX_RATE) + ' M/s', True, green if RX_RATE > 0 else white)
+    netSpeedOutText = tinyFont.render('' + str(TX_RATE) + ' M/s', True, green if TX_RATE > 0 else white)
 
     ip = get_host_ip()
     ipText = miniFont.render(ip, True, white)
     
     screen.blit(cputempText, (10,0))
+    screen.blit(cpuUseText, (w - cpuUseText.get_width() - 2,0))
     screen.blit(timeText, (4,16))
     screen.blit(secondText, (190, 52))
     screen.blit(monthText, (15,86))
     # screen.blit(yearText, (145,120))
     screen.blit(amText,(190,22))
     screen.blit(dayText,(170,86))
-    screen.blit(ipText,(15,112))
-
+    # if netShowType == 0:
+    #     screen.blit(ipText,(w - ipText.get_width(), 112))
+    # else:
+    #     screen.blit(netSpeedOutText, (w - netSpeedOutText.get_width(), 112))
+    #     screen.blit(netSpeedInText, (w - netSpeedInText.get_width() - netSpeedOutText.get_width() - 10, 112))
+    screen.blit(ipText,(10, 112))
+    screen.blit(netSpeedOutText, (w - netSpeedOutText.get_width(), 112))
+    
+    prevSecondIntValue = secondIntValue
     pygame.display.update()
     time.sleep(1.0/24.0)
